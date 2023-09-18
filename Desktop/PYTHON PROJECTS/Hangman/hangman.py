dd4f52def3d35123e62ca29c908936e94a712279
Anne C <anne.cadeillan@gmail.com>
2023-09-18 21:27:21 +0200

    layout

---
+++ Desktop/PYTHON PROJECTS/Hangman/hangman.py
@@ -0,0 +1,14 @@
+#l'utilisateur choisit un nombre de lettres
+
+#choisir un mot au hasard en français
+
+#en fonction du nombre de lettres dans le mot, l'ordinateur dessine x nombre de tirets
+
+#l'ordi demande à l'utilisateur de choisir une lettre
+
+#si cette lettre ne convient pas, la potence continue à se dessiner
+
+#si elle convient, elle apparaît au dessus des tirets
+
+#si l'utilisateur se trompe 7 fois, il a perdu.
+
